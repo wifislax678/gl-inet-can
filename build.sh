391ef05fd1a29f5e8cfdf494d29556d0a3e12221
duliang123 <5918983@qq.com>
2022-07-24 13:04:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ function main() {
     ./scripts/gen_config.py target_wlan_ap-gl-axt1800 glinet_depends
     
     git clone https://github.com/gl-inet/glinet4.x.git
+    ./scripts/feeds update -a
+    ./scripts/feeds install -a
+    make defconfig
     make -j$(expr $(nproc) + 1) GL_PKGDIR=$BASE_DIR/glinet/work/glinet4.x/ipq60xx/
 }
 
