2c89bb457f9ff4981ea04ab79306305eedf55e23
duliang123 <5918983@qq.com>
2022-07-24 14:25:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,14 @@ BASE_DIR=$(pwd)
 
 function main() {
     git clone https://github.com/gl-inet/gl-infra-builder.git  $BASE_DIR/gl-infra-builder
+    cp -r $PWD/*.yml $PWD/gl-infra-builder/profiles
+    
     cd $BASE_DIR/gl-infra-builder
     python3 setup.py -c config-wlan-ap.yml
+    
     cd wlan-ap/openwrt
     ./scripts/gen_config.py target_wlan_ap-gl-axt1800 glinet_depends
+    
     
     git clone https://github.com/gl-inet/glinet4.x.git
     ./scripts/feeds update -a
