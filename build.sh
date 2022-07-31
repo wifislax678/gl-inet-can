414476bbfcfa3867d92f62b6698e2f63159b552b
duliang123 <5918983@qq.com>
2022-07-31 13:55:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/bin/env bash
 
 sudo mkdir /workspace && sudo chown $USER:$GROUPS /workspace
-# BASE_DIR=$(pwd)
-BASE_DIR="/workspace"
-cd $BASE_DIR
+# WORK_PATH=$(pwd)
+WORK_PATH="/workspace"
+cd $WORK_PATH
 
 function print_line() {
     echo '###############################################################################'
 }
 
 function main() {
-    git clone https://github.com/gl-inet/gl-infra-builder.git  $BASE_DIR/gl-infra-builder
-    cp -r $BASE_DIR/*.yml $BASE_DIR/gl-infra-builder/profiles
+    git clone https://github.com/gl-inet/gl-infra-builder.git  $WORK_PATH/gl-infra-builder
+    cp -r $WORK_PATH/*.yml $WORK_PATH/gl-infra-builder/profiles
     
-    cd $BASE_DIR/gl-infra-builder
+    cd $WORK_PATH/gl-infra-builder
     python3 setup.py -c config-wlan-ap.yml
     
     cd wlan-ap/openwrt
     print_line
-    echo $BASE_DIR
+    echo $WORK_PATH
     print_line
     ls -lh
     print_line
@@ -27,12 +27,12 @@ function main() {
     ./scripts/gen_config.py glinet-axt1800 glinet_depends
 
     
-    git clone https://github.com/gl-inet/glinet4.x.git -b main $BASE_DIR/glinet
+    git clone https://github.com/gl-inet/glinet4.x.git -b main $WORK_PATH/glinet
     ./scripts/feeds update -a
     ./scripts/feeds install -a
     make defconfig
-    # make -j$(expr $(nproc) + 1) GL_PKGDIR=$BASE_DIR/glinet/ipq60xx/ V=s
-    make -j1 GL_PKGDIR=$BASE_DIR/glinet/ipq60xx/ V=s
+    # make -j$(expr $(nproc) + 1) GL_PKGDIR=$WORK_PATH/glinet/ipq60xx/ V=s
+    make -j1 GL_PKGDIR=$WORK_PATH/glinet/ipq60xx/ V=s
 }
 
 main
